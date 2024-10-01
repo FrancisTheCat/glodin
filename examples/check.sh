00cccc02c2ada607a4b06ec5db0a13392d258e53
Franz Hoeltermann <francis_the_cat@gmx.de>
2024-10-01 15:19:07 +0200

    Removed example binaries and improved `check.sh`

--- examples/check.sh
+++ examples/check.sh
@@ -4,31 +4,4 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 cd "$parent_path"
 
-echo "Checking shadows"
-odin check shadows --show-timings
-echo ""
-
-echo "Checking instancing"
-odin check instancing --show-timings
-echo ""
-
-echo "Checking bloom"
-odin check bloom --show-timings
-echo ""
-
-echo "Checking compute"
-odin check compute --show-timings
-echo ""
-
-echo "Checking textured_cube"
-odin check textured_cube --show-timings
-echo ""
-
-echo "Checking conway"
-odin check conway --show-timings
-echo ""
-
-echo "Checking canvas"
-odin check canvas --show-timings
-echo ""
-
+for d in ./*/ ; do (cd "$d" && echo "checking $d" && odin check . && echo ""); done
